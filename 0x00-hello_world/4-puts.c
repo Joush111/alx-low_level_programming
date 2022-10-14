64ca67bb8a5c00716a51f75e68a16d4528282099
Joush111 <tony.joush@protonmail.com>
2022-10-14 06:41:41 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- * * main - Entry point
- * * Description: prints "Programming is like building a multilingual puzzle"
- * * Return: Always 0 (Success)
- * */
+ * main - Entry
+ * Return: Always 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
