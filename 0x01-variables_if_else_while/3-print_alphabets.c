3a0b54321d492edd0f3196c61326b1903df99e17
Joush111 <tony.joush@protonmail.com>
2022-10-16 18:16:07 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ * @void: parameter
+ * Return: 0 (sucess)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	for (j = 65; j < 91; j++)
+	{
+		putchar(j);
+	}
+	putchar(10);
+
+	return (0);
+}
