6b56d3d70c53f4c26739636b1d8be2e38a4d0744
Joush111 <tony.joush@protonmail.com>
2022-10-16 17:40:42 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @void: parameter
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putcher(i);
+	}
+
+	putchar(10);
+
+	return (0);
+}
